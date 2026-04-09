eec452958488cab176f2911cfd9c8c17a639e4c0
Victor Faleiros <victorgui08@gmail.com>
2026-04-09 10:37:25 -0300

    Ignore

--- PrimeiroPrograma.py
+++
@@ -1,5 +0,0 @@
-print("Olá, Mundo!")
-print("\nPython é uma linguagem simples, mas que incita a criatividade, uma caixa de ferramentas que liberta a mente.")
-
-resposta = str(input("Você concorda? "))
-print("Como é bom poder programar!\n")
