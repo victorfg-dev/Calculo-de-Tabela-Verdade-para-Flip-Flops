92adabd2f17cd1b96cf6f2b082a03f9f8947e55b
victorfg-dev <victorgui08@gmail.com>
2026-04-09 10:15:55 -0300

    Versão 0.02

---
+++ PrimeiroPrograma.py
@@ -0,0 +1,5 @@
+print("Olá, Mundo!")
+print("\nPython é uma linguagem simples, mas que incita a criatividade, uma caixa de ferramentas que liberta a mente.")
+
+resposta = str(input("Você concorda? "))
+print("Como é bom poder programar!\n")
